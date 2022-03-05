69d72f77df5facd7cbdbc549facb69291ce0e289
Adrian Acedo <adrian@coderei.dev>
2022-03-05 14:44:47 +0100

    feat: food search1

--- src/logic.js
+++ src/logic.js
@@ -41,12 +41,8 @@ const goDown = myHead => ({ x: myHead.x, y: myHead.y - 1 })
 const goRight = myHead => ({ x: myHead.x + 1, y: myHead.y })
 const goLeft = myHead => ({ x: myHead.x - 1, y: myHead.y })
 const compareCoordinates = (positionA, positionB) => (positionA.x === positionB.x && positionA.y === positionB.y)
-// const compareCoordinates = (positionA, positionB) => {
-//   console.log(positionA.x, positionA.y)
-//   console.log(positionB.x, positionB.y)
-//   console.log((positionA.x === positionB.x && positionA.y === positionB.y))
-//   return (positionA.x === positionB.x && positionA.y === positionB.y)
-// }
+const distanceBetween = (positionA, positionB) => (Math.abs(positionA.x - positionB.x) + Math.abs(positionA.y - positionB.y))
+
 
 const nextPositions = (myHead, possibleMoves) => {
   const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key])
@@ -55,16 +51,16 @@ const nextPositions = (myHead, possibleMoves) => {
   safeMoves.forEach(safeMove => {
 
     if (safeMove === 'up') {
-      nextPositionArray.push(goUp(myHead))
+      nextPositionArray.push({ ...goUp(myHead), movement: 'up' })
     }
     if (safeMove === 'down') {
-      nextPositionArray.push(goDown(myHead))
+      nextPositionArray.push({ ...goDown(myHead), movement: 'down' })
     }
     if (safeMove === 'right') {
-      nextPositionArray.push(goRight(myHead))
+      nextPositionArray.push({ ...goRight(myHead), movement: 'right' })
     }
     if (safeMove === 'left') {
-      nextPositionArray.push(goLeft(myHead))
+      nextPositionArray.push({ ...goLeft(myHead), movement: 'left' })
     }
     return nextPositionArray
   })
@@ -94,10 +90,16 @@ function move(gameState) {
     left: true,
     right: true
   }
+  const resetMovement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+  }
   const boardWidth = gameState.board.width
   const boardHeight = gameState.board.height
 
-  // Step 0: Don't let your Battlesnake move back on its own neck
+  // Step 0: Don't let your snake move back on its own neck
   const myHead = gameState.you.head
   const myNeck = gameState.you.body[1]
   if (myNeck.x < myHead.x) {
@@ -114,33 +116,16 @@ function move(gameState) {
   console.log('heady: ', myHead.y)
 
   // Step 1 - Don't hit walls.
-  // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
+  // Use information in gameState to prevent your snake from moving beyond the boundaries of the board.
   possibleMoves = dodgeWalls(myHead, possibleMoves, boardWidth, boardHeight)
 
 
-
   // Step 2 - Don't hit yourself.
-  // Use information in gameState to prevent your Battlesnake from colliding with itself.
-  // const myBody = gameState.you.body
-  // myBody.forEach(myPart => {
-  //   if (compareCoordinates(myPart, goUp(myHead))) {
-  //     possibleMoves.up = false
-  //   }
-  //   if (compareCoordinates(myPart, goDown(myHead))) {
-  //     possibleMoves.down = false
-  //   }
-  //   if (compareCoordinates(myPart, goRight(myHead))) {
-  //     possibleMoves.right = false
-  //   }
-  //   if (compareCoordinates(myPart, goLeft(myHead))) {
-  //     possibleMoves.left = false
-  //   }
-  // })
-
+  // Use information in gameState to prevent your snake from colliding with itself.
   // Step 3 - Don't collide with others.
-  // Use information in gameState to prevent your Battlesnake from colliding with others.
-  gameState.board.snakes.forEach((snake, index) => {
-    // First snake is my snake
+  // Use information in gameState to prevent your snake from colliding with others.
+  gameState.board.snakes.forEach(snake => {
+    // First snake is my snake, so step 2 & 3 are treated here
     if (snake) {
       console.log('name: ', JSON.stringify(snake.name))
       snake.body.forEach(snakePart => {
@@ -163,10 +148,17 @@ function move(gameState) {
     }
   })
 
-  // const nextPositionArray = nextPositions(myHead, possibleMoves)
 
   // TODO: Step 4 - Find food.
   // Use information in gameState to seek out and find food.
+  const food = gameState.board.food
+  const closestFood = Math.min(food.map(foodPoint => distanceBetween(foodPoint, myHead)))
+  const nextPointsToMove = nextPositions(myHead, possibleMoves)
+  const favoritePoint = Math.min(nextPointsToMove.map(nextPoint => distanceBetween(nextPoint, closestFood)))
+
+  // const favoriteDirection = favoritePoint.movement;
+  possibleMoves = { ...resetMovement, [favoritePoint.movement]: true }
+
 
   // Finally, choose a move from the available safe moves.
   // TODO: Step 5 - Select a move to make based on strategy, rather than random.
